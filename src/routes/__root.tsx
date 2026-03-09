87c5da4d3a0655e367ec30aa067973192f5a7e22
Salah Mohamed <salah.mohamed0404@gmail.com>
2026-03-09 16:28:17 +0200

    feat: initial commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,30 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+import '../styles.css'
+import { Header } from '#/components/layout/header'
+
+export const Route = createRootRoute({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'TanStack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+        ]}
+      />
+    </>
+  )
+}
